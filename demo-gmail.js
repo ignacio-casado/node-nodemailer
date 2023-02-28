44fce5c0f39f85cf973b76f9e7fbbff34780b87a
Ignacio casado <nacho.casado01@gmail.com>
2023-02-28 14:46:14 -0300

    change array

--- demo-gmail.js
+++ demo-gmail.js
@@ -1,13 +1,13 @@
 import { createTransport } from "nodemailer";
 
-const TEST_MAIL = "marcia.hoppe@ethereal.email"
+const TEST_MAIL = "nacho.casado01@gmail.com"
 
 const transporter = createTransport({
     service: 'gmail',
     port: 587,
     auth: {
         user: TEST_MAIL,
-        pass: 'WC9kSmFgCqbC2aenj8'
+        pass: '???????' //check https://www.google.com/url?q=https://security.google.com/settings/security/apppasswords&sa=D&source=editors&ust=1677606208202364&usg=AOvVaw16-GAORupKO3CJEP3QK4bF
     }
 });
 
